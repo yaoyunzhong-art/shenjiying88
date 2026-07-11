d71a473e9bbdac164383ec69c92875bb0aac281a
yaoyunzhong <2810264498@qq.com>
2026-07-12 02:39:01 +0800

    🧹 移除旧项目sports-ant-saas遗留文件

--- sports-ant-saas/backend/dist/src/modules/venues/dto/update-venue.dto.d.ts
+++
@@ -1,5 +0,0 @@
-import { CreateVenueDto } from './create-venue.dto';
-declare const UpdateVenueDto_base: import("@nestjs/common").Type<Partial<CreateVenueDto>>;
-export declare class UpdateVenueDto extends UpdateVenueDto_base {
-}
-export {};
--- sports-ant-saas/backend/src/modules/venues/dto/update-venue.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateVenueDto } from './create-venue.dto';
-
-export class UpdateVenueDto extends PartialType(CreateVenueDto) {}
