7f02b73195f9fdab7a45b0ffb122deefbf5c85cd
yaoyunzhong <2810264498@qq.com>
2026-07-24 23:53:14 +0800

    feat(minor-protection): 注册到app.module (未成年保护正式可用)

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -162,6 +162,7 @@ import { CategoriesModule } from './modules/categories/categories.module';
 import { TeamBuildingModule } from './modules/team-building/team-building.module';
 import { HrModule } from './modules/hr/hr.module';
 import { BrandOperationsModule } from './modules/brand-operations/brand-operations.module';
+import { MinorProtectionModule } from './modules/minor-protection/minor-protection.module';
 import { StoreModule } from './modules/store/store.module';
 import { FeedbackModule } from './modules/feedback/feedback.module';
 import { ProbationTransferModule } from './modules/transfer/probation-transfer.module';
@@ -342,6 +343,7 @@ import { OpenPlatformModule } from './modules/open-platform/open-platform.module
     TeamBuildingModule,
     HrModule,
     BrandOperationsModule,
+    MinorProtectionModule,
     StoreModule,
     FeedbackModule,
     ProbationTransferModule,
